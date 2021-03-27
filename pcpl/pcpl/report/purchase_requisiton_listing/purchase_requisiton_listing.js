b897478a7de1cd45436df8e0150865ba76452c8a
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-03-27 10:01:20 +0530

    Update purchase_requisiton_listing.js

--- pcpl/pcpl/report/purchase_requisiton_listing/purchase_requisiton_listing.js
+++ pcpl/pcpl/report/purchase_requisiton_listing/purchase_requisiton_listing.js
@@ -4,6 +4,25 @@
 
 frappe.query_reports["Purchase Requisiton Listing"] = {
 	"filters": [
-
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item"
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"reqd": 1
+		}
 	]
 };
