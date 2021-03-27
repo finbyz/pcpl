1438b3a8537a8d9a8c7ef39c95b9c22c3c547a22
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-03-27 10:06:23 +0530

    Update material_receipt_listing.js

--- pcpl/pcpl/report/material_receipt_listing/material_receipt_listing.js
+++ pcpl/pcpl/report/material_receipt_listing/material_receipt_listing.js
@@ -4,6 +4,38 @@
 
 frappe.query_reports["Material Receipt Listing"] = {
 	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"supplier",
+			"label": __("Supplier"),
+			"fieldtype": "Link",
+			"options": "Supplier"
+		},
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item"
+		},
+		{
+			"fieldname":"item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		},
 
 	]
 };
