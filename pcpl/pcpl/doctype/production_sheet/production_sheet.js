b93be403628d8b3e2cf78d541bde62e4c5ae4d0d
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-04-01 13:14:20 +0530

    Delete pcpl/pcpl/doctype/production_sheet directory

--- pcpl/pcpl/doctype/production_sheet/production_sheet.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, sprics and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Production Sheet', {
-	// refresh: function(frm) {
-
-	// }
-});
