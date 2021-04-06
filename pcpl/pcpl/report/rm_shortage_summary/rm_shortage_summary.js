b04ce3ef637766d8d6932e0a1d1e2c11b6cda27d
nishchay2698 <nishchay.garala@gmail.com>
2021-04-06 03:18:50 +0000

    rm shortage report

---
+++ pcpl/pcpl/report/rm_shortage_summary/rm_shortage_summary.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, STPL and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["RM Shortage Summary"] = {
+	"filters": [
+
+	]
+};
