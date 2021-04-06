6f93e0b0f679ba4ce37c9e3e73f730d75d54272c
nishchay2698 <67817531+nishchay2698@users.noreply.github.com>
2021-04-06 08:51:08 +0530

    Update rm_shortage_summary.js

--- pcpl/pcpl/report/rm_shortage_summary/rm_shortage_summary.js
+++ pcpl/pcpl/report/rm_shortage_summary/rm_shortage_summary.js
@@ -4,6 +4,36 @@
 
 frappe.query_reports["RM Shortage Summary"] = {
 	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse"
+		},
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"get_query": function() {
+				return {
+					query: "erpnext.controllers.queries.item_query"
+				}
+			}
+		},
+		{
+			"fieldname":"item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		}
 
 	]
 };
