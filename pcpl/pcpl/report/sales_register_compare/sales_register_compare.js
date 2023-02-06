bfd676155260417cbd67d250c085d4de0ee5f67f
Ubuntu <ubuntu@ip-172-31-16-105.ap-south-1.compute.internal>
2023-02-06 04:48:55 +0000

    new report added

---
+++ pcpl/pcpl/report/sales_register_compare/sales_register_compare.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, STPL and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Register Compare"] = {
+	"filters": [
+
+	]
+};
